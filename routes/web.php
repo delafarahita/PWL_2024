b1b0d2129b5f26f0f3a7e4343f1d27b41f13539f
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-02-20 11:46:16 +0700

    Basic Routing - Jobsheet2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241720058 - Dela Farahita Zain';
 });
