113b5c8fcc7ea6045f7f0ad75f3f0167e1a8a38b
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-02-20 12:10:20 +0700

    Optional Parameters - Jobsheet2

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,13 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
 Route::get('/articles/{id}', function ($articlesId) {
     return 'Halaman Artikel dengan ID ' . $articlesId;
 });
+
+// Optional Parameters
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
